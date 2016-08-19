4a6d6c980b549a8fb29909cfa69ff7851d8c93e6
marcel.koopman <marcel.koopman@gmail.com>
2016-08-19 22:30:07 +0200

    Initial commit

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-val akkaVersion = "2.4.9-RC2"
+val akkaVersion = "2.4.9"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % akkaVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % akkaVersion
--- src/main/scala/flow/ServiceActor.scala
+++ src/main/scala/flow/ServiceActor.scala
@@ -4,6 +4,9 @@ import akka.actor.{Actor, ActorLogging, Props}
 import com.github.marcelkoopman.actorflow.flow.Orchestrator
 import com.github.marcelkoopman.actorflow.flow.Orchestrator.{FinishedWorkEvt, WorkMsg}
 
+import scala.concurrent.Future
+import scala.util.{Failure, Random, Success}
+
 /**
   * Created by marcel on 14-8-2016.
   */
@@ -13,14 +16,22 @@ object ServiceActor {
 
 private class ServiceActor extends Actor with ActorLogging {
 
+  import context.dispatcher
+
+
   def receive = {
     case msg:WorkMsg => {
       log.info("I am "+this +" got msg " + msg.str)
 
-      //throw new NullPointerException("BOOM")
+      val f = Future {
+        throw new NullPointerException("BOOM")
+      }.onFailure {
+        case e:NullPointerException => self ! e
+      }
 
-      sender ! FinishedWorkEvt(msg.str)
+      sender ! "OK"
     }
+    case ex:Exception => throw ex
   }
 
   override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
